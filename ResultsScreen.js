51d5a5d495687347bb45879f4410dcb24faff321
tokinonagare <timemahou@gmail.com>
2015-10-21 13:47:00 +0800

    State Judge for Loading

--- ResultsScreen.js
+++ ResultsScreen.js
@@ -14,7 +14,12 @@ var buildUrl = function(q) {
 };
 
 var ResultsScreen = React.createClass({
-	
+	getInitialState: function() {
+		return {
+			isLoading: true,
+		};
+	},
+
 	componentDidMount: function() {
     this.fetchResults(this.props.searchPhrase);
   },
@@ -22,22 +27,44 @@ var ResultsScreen = React.createClass({
   fetchResults: function(searchPhrase) {
     fetch(buildUrl(searchPhrase))
     .then(response => response.json())
-    .then(jsonData => console.dir(jsonData))
+    .then(jsonData => {
+    	this.setState({isLoading: false });
+    	console.dir(jsonData);
+    })
     .catch(error   => console.dir(error));
   },
 
-	render: function() {
+  render: function() {
+  	if (this.state.isLoading) {
+  		return this.renderLoadingMessage();
+  	} else {
+  		return this.renderResults();
+  	}
+  },
+
+	renderLoadingMessage: function() {
 		return (
-			<View style = {styles.container} >
-				<Text style = {styles.label} >
-					This is the Results screen
-				</Text>
+			<View style = {styles.container}>
 				<Text style = {styles.label}>
-					You searched for: {this.props.searchPhrase}
+					Searching for "{this.props.searchPhrase}".
+				</Text>
+				<Text style={styles.label}>
+					Please wait...
 				</Text>
 			</View>
 		);
-	}
+	},
+
+	renderResults: function() {
+		return(
+			<View style={styles.container}>
+				<Text style={styles.label}>
+					Finished searching.
+				</Text>
+			</View>
+		);
+	},
+
 });
 
 var styles = StyleSheet.create({
